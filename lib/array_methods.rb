8d180b2460e39dc48bfefa578c17974953ae7579
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-06 23:36:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = 0
+  max = array[0]
     array.length.times do |index|
       if array[index] > max
         max = array[index]
@@ -20,6 +20,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
+  min = array[0]
     array.length.times do |index|
       if array[index] < min
         min = array[index]
